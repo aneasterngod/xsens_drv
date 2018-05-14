9d6cbcf79f3cc1d3f81f2f54e073b1c934ceba9b
aneasterngod <38875154+aneasterngod@users.noreply.github.com>
2018-05-14 21:53:58 +0900

    Delete xti300Config.cmake

--- src/xti300Config.cmake
+++
@@ -1,4 +0,0 @@
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/xti300-targets.cmake)
-get_filename_component(xti300_INCLUDE_DIRS "${SELF_DIR}/../../include" ABSOLUTE)
-get_filename_component(xti300_LIBRARY_DIRS "${SELF_DIR}/../../lib" ABSOLUTE)
